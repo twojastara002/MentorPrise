3588944276b38a174acf722e15e38f70ca79a0b7
Karol Bura <karol.bura544@gmail.com>
2023-04-06 17:43:49 +0100

    props example

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,20 @@ import ListC4 from "./components/testing_and_cheatsheets/ListC4";
 import ListC5 from "./components/testing_and_cheatsheets/ListC5";
 
 function App() {
+  let items2 = ["l1", "sdgkds", "45839", "5235"];
+
+  const handleSelectItem = (item: string) => {
+    console.log("from the App: sdojgnskjdgn");
+  };
+
   return (
     <>
       <ListC4 />
-      <ListC5 />
+      <ListC5
+        items={items2}
+        heading="AAAAAAAAAAAAA"
+        onSelectItem={handleSelectItem}
+      />
     </>
   );
 }
