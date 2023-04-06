9f78fea507ddaa65e517a6f8e1c926fef74f96e8
Karol Bura <karol.bura544@gmail.com>
2023-04-06 17:43:22 +0100

    props example

--- src/components/testing_and_cheatsheets/ListC5.tsx
+++ src/components/testing_and_cheatsheets/ListC5.tsx
@@ -1,39 +1,56 @@
 import { MouseEvent } from "react";
 import { useState } from "react";
 
-function ListC5() {
+interface ListC5Props {
+  items: string[];
+  heading: string;
+  onSelectItem: (item: string) => void;
+  /*create a signature to the function that will be later referenced when item is selected and can be passed by @App.tsx */
+}
+/*when calling this component the things inside the interface passed have to be passed like html attributes
+ like so: <ListC5 items={items2} heading="AAAAAAAAAAAAA" /> */
+
+function ListC5({ items, heading, onSelectItem }: ListC5Props) {
+  /*props has been destructured so that it doesn't need to be called like props.items every time */
   let items2 = ["a", "b", "c", "5235"];
 
   let [selectedIndex, setSelectedIndex] = useState(-1);
-  /*returns a two value array, i0 is the value of the variable, i1 is the function that will be executed when i0 is changed to re-render the components */
-  /*creating the variable like that lets you call the @selectedIndex and it's render function separately */
 
-  const handleClicked = (event: MouseEvent, item: String, index: number) => {
+  const handleClicked = (event: MouseEvent, item: string, index: number) => {
     setSelectedIndex(index);
-    /*pass the new value to the re-rendering function when you want to change @selectedIndex */
+    onSelectItem(item);
   };
   /*function to handle events, to get data from the event call methods or attributes of the event object,
        to get more accurate stuff use a more accurate event, like mouse event */
 
   return (
     <>
-      <h1>AAAA</h1>
+      <h1>
+        {
+          heading /*reference to the props element passed after it was destructured */
+        }
+      </h1>
       <ul className="list-group">
-        {items2.map((item, index) /*works like a for each loop */ => (
-          <li
-            key={item}
-            className={
-              selectedIndex === index
-                ? "list-group-item active"
-                : "list-group-item"
-            }
-            onClick={(event) => handleClicked(event, item, index)}
-            /*component property that is responsible for item being clicked, to have more than one parameter here use this format
+        {items.map(
+          (
+            item,
+            index
+          ) /*works like a for each loop, items is props.items */ => (
+            <li
+              key={item}
+              className={
+                selectedIndex === index
+                  ? "list-group-item active"
+                  : "list-group-item"
+              }
+              onClick={(event) => handleClicked(event, item, index)}
+              /*component property that is responsible for item being clicked, to have more than one parameter here use this format
                  with event creating an arrow function and the rest of the parameters being passed later */
-          >
-            {item}
-          </li>
-        ))}
+            >
+              {item}
+            </li>
+          )
+        )}
       </ul>
     </>
   );
