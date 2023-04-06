a66054e0dc23cb7cbff2e0c027f8f24c035a19d5
Karol Bura <karol.bura544@gmail.com>
2023-04-06 17:21:00 +0100

    dasfaf

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import ListC from "./components/ListC";
+import ListC from "./components/testing_and_cheatsheets/ListC";
+import ListC4 from "./components/testing_and_cheatsheets/ListC4";
+import ListC5 from "./components/testing_and_cheatsheets/ListC5";
 
 function App() {
   return (
-    <ListC/>
-  )
+    <>
+      <ListC4 />
+      <ListC5 />
+    </>
+  );
 }
 
-export default App
+export default App;
