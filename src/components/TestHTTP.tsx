51e4fd4074baa4c16bdd780eaa59f38faf10d124
Karol Bura <karol.bura544@gmail.com>
2023-04-11 05:31:47 +0100

    loggin with server files

--- src/components/TestHTTP.tsx
+++ src/components/TestHTTP.tsx
@@ -2,12 +2,12 @@ import { useState } from "react";
 
 function TestHTTP() {
   let [updateText, setUpdateText] = useState("");
-
   var xhr = new XMLHttpRequest();
+
   xhr.onload = () => {
     setUpdateText(xhr.responseText);
   };
-  xhr.open("GET", "http://localhost:3000/user/514");
+  xhr.open("GET", "http://localhost:3000/login/u1-password");
   xhr.send();
 
   return <div>{updateText}</div>;
