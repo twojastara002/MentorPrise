207e1afce92308ac6a9607f8c5f073a26f4856ce
Ahmed-Hashi <94930699+Ahmed-Hashi@users.noreply.github.com>
2023-04-12 20:42:29 +0100

    Update UserProfile.tsx

--- src/components/UserProfile.tsx
+++ src/components/UserProfile.tsx
@@ -11,8 +11,10 @@ const UserProfile: React.FC<UserProfileProps> = ({ email, name, onLogout }) => {
     return (
         <div>
             <h1>User Profile</h1>
-            <p>Name: {name}</p>
-            <p>Email: {email}</p>
+            <Col>Name: {name}</Col>
+            <Col>Email: {email}</Col>
+            <Col>Departmart: {mentors.department}</Col>
+            <Col>Years of Experience: {mentors.YearsOfExperience}</Col>
         </div>
     );
 };
