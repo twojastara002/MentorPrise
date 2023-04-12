7fa5a8ee30dc06f5a14114645ab5ba63e12c920f
Karol Bura <karol.bura544@gmail.com>
2023-04-12 23:23:39 +0100

    fixed profile

--- src/components/UserProfile.tsx
+++ src/components/UserProfile.tsx
@@ -1,22 +1,22 @@
-import '../css/profile.css
+import "../css/profile.css";
 import React from "react";
 
 interface UserProfileProps {
-    email: string;
-    name: string;
-    onLogout: () => void;
+  email: string;
+  name: string;
+  onLogout: () => void;
 }
 
 const UserProfile: React.FC<UserProfileProps> = ({ email, name, onLogout }) => {
-    return (
-        <div>
-            <h1>User Profile</h1>
-            <Col>Name: {name}</Col>
-            <Col>Email: {email}</Col>
-            <Col>Departmart: {mentors.department}</Col>
-            <Col>Years of Experience: {mentors.YearsOfExperience}</Col>
-        </div>
-    );
+  return (
+    <div>
+      <h1>User Profile</h1>
+      <p>Name: {name}</p>
+      <p>Email: {email}</p>
+      {/* <p>Departmart: {mentors.department}</p>
+            <p>Years of Experience: {mentors.YearsOfExperience}</p> */}
+    </div>
+  );
 };
 
 export default UserProfile;
