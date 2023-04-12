d4df3fd53ace28b105d1fb2bcd8d078111825346
Karol Bura <karol.bura544@gmail.com>
2023-04-12 05:24:43 +0100

    fixed login again

--- src/components/RegistrationForm.tsx
+++ src/components/RegistrationForm.tsx
@@ -3,86 +3,83 @@ import TriggerTextBox from "./TriggerTextBox";
 import Button from "./Button";
 
 interface RegistrationItems {
-    onRegistrationSuccess: (name: string, email: string, password: string) => void;
+  onRegistrationSuccess: (
+    name: string,
+    email: string,
+    password: string
+  ) => void;
 }
 
 const Registration: React.FC<RegistrationItems> = ({
-    onRegistrationSuccess,
+  onRegistrationSuccess,
 }) => {
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [confirmPassword, setConfirmPassword] = useState("");
-    const [errorMessage, setErrorMessage] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
-    const handleNameChange = (value: string) => {
-        setName(value);
-    };
+  const handleNameChange = (value: string) => {
+    setName(value);
+  };
 
-    const handleEmailChange = (value: string) => {
-        setEmail(value);
-    };
+  const handleEmailChange = (value: string) => {
+    setEmail(value);
+  };
 
-    const handlePasswordChange = (value: string) => {
-        setPassword(value);
-    };
+  const handlePasswordChange = (value: string) => {
+    setPassword(value);
+  };
 
-    const handleConfirmPasswordChange = (value: string) => {
-        setConfirmPassword(value);
-    };
+  const handleConfirmPasswordChange = (value: string) => {
+    setConfirmPassword(value);
+  };
 
-    const handleRegisterClick = () => {
+  const handleRegisterClick = () => {
+    if (!/\S+@\S+\.\S+/.test(email)) {
+      setErrorMessage("Please enter a valid email address.");
+      return;
+    }
 
-        if (!/\S+@\S+\.\S+/.test(email)) {
-            setErrorMessage("Please enter a valid email address.");
-            return;
-        }
+    if (!name || !email || !password || !confirmPassword) {
+      setErrorMessage("All fields are required");
+      return;
+    }
+    if (password !== confirmPassword) {
+      setErrorMessage("Passwords do not match");
+      return;
+    }
 
-        if (!name || !email || !password || !confirmPassword) {
-            setErrorMessage('All fields are required');
-            return;
-        }
-        if (password !== confirmPassword) {
-            setErrorMessage("Passwords do not match");
-            return;
-        }
+    // Perform validation on the email and password
 
-        // Perform validation on the email and password
+    // Assuming validation passes
+    onRegistrationSuccess(name, email, password);
+  };
 
-        // Assuming validation passes
-        onRegistrationSuccess(name, email, password);
-    };
-
-    return (
-        <div>
-            <TriggerTextBox
-                label="Name"
-                value={name}
-                onChange={handleNameChange}
-            />
-            <TriggerTextBox
-                label="Email"
-                value={email}
-                onChange={handleEmailChange}
-            />
-            <TriggerTextBox
-                label="Password"
-                value={password}
-                onChange={handlePasswordChange}
-                isPassword
-            />
-            <TriggerTextBox
-                label="Confirm Password"
-                value={confirmPassword}
-                onChange={handleConfirmPasswordChange}
-                isPassword
-            />
-            {errorMessage && <div className="error">{errorMessage}</div>}
-            <Button label="Register" onClick={handleRegisterClick} />
-        </div>
-    );
+  return (
+    <div>
+      <TriggerTextBox label="Name" value={name} onChange={handleNameChange} />
+      <TriggerTextBox
+        label="Email"
+        value={email}
+        onChange={handleEmailChange}
+      />
+      <TriggerTextBox
+        label="Password"
+        value={password}
+        onChange={handlePasswordChange}
+        isPassword
+      />
+      <TriggerTextBox
+        label="Confirm Password"
+        value={confirmPassword}
+        onChange={handleConfirmPasswordChange}
+        isPassword
+      />
+      {errorMessage && <div className="error">{errorMessage}</div>}
+      <Button label="Register" onClick={handleRegisterClick} />
+    </div>
+  );
 };
 
 export default Registration;
-
-
